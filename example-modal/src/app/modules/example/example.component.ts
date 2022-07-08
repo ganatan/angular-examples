6dd2b0a7d8bc52cfd236bc55d582ec3f6a120102
ganatan <dannyganatan@gmail.com>
2022-07-08 16:10:05 +0200

    Add modal example

---
+++ example-modal/src/app/modules/example/example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css']
+})
+export class ExampleComponent {
+
+  constructor() { }
+
+}
---
+++ example-modal/src/app/modules/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private meta: Meta) {
+  }
+
+  ngOnInit(): void {
+
+    this.meta.updateTag(
+      {
+        name: 'description',
+        content: 'This application was developed with Angular version 14.0.5 and bootstrap 5.1.3' +
+          ' It applies Routing, Lazy loading, Server side rendering and Progressive Web App (PWA)'
+      });
+
+  }
+
+}
